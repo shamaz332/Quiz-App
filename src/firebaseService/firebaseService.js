001e55e44e4d38a0f3fbd30416805aeae82b874f
shamaz332 <shamazsaeed332@gmail.com>
2020-08-30 19:30:01 +0500

    whitespace error resolve

--- src/firebaseService/firebaseService.js
+++ src/firebaseService/firebaseService.js
@@ -1,22 +1,20 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCMNjO4BbO5BnT2TfjxAMCJAazaT0-M4iw",
-    authDomain: "quizy-983a3.firebaseapp.com",
-    databaseURL: "https://quizy-983a3.firebaseio.com",
-    projectId: "quizy-983a3",
-    storageBucket: "quizy-983a3.appspot.com",
-    messagingSenderId: "201474915525",
-    appId: "1:201474915525:web:b62681e187dce7344744ac"
-  };
+  apiKey: "AIzaSyCMNjO4BbO5BnT2TfjxAMCJAazaT0-M4iw",
+  authDomain: "quizy-983a3.firebaseapp.com",
+  databaseURL: "https://quizy-983a3.firebaseio.com",
+  projectId: "quizy-983a3",
+  storageBucket: "quizy-983a3.appspot.com",
+  messagingSenderId: "201474915525",
+  appId: "1:201474915525:web:b62681e187dce7344744ac",
+};
 
-
-// auto notification 
+// auto notification
 function randomNotification() {
-
-    var notifTitle = "Hi there"
-    var notif = new Notification(notifTitle);
-    setTimeout(randomNotification, 30000);
+  var notifTitle = "Hi there";
+  new Notification(notifTitle);
+  setTimeout(randomNotification, 30000);
 }
 // auto notification  end
 firebase.initializeApp(firebaseConfig);
@@ -27,7 +25,7 @@ export function requestPermission() {
   // [START request_permission]
   Notification.requestPermission().then((permission) => {
     if (permission === "granted") {
-        randomNotification();
+      randomNotification();
       messaging
         .getToken()
         .then((currentToken) => {
@@ -36,11 +34,10 @@ export function requestPermission() {
           } else {
             console.log("no id");
           }
-        }) .catch((err)=>
-        {
-          console.log(err)
         })
-       
+        .catch((err) => {
+          console.log(err);
+        });
     }
   });
 }
